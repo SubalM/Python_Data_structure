96684d20683dd1cceffcd3c4f6f487dde7f82ece
Subal Murugan <126765878+SubalM@users.noreply.github.com>
2025-06-17 15:16:09 +0530

    Add files via upload

---
+++ group_anagram.py
@@ -0,0 +1,22 @@
+# method_1
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            sortedS = ''.join(sorted(s))
+            print(sortedS)
+            res[sortedS].append(s)
+            print(res)
+        return list(res.values())
+
+# method_2:
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
+        return list(res.values())
+
